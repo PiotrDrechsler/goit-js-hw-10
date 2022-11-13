fecf664724f54f312bc68c7e821d763d6a84a23c
PiotrDrechsler <pdrech@gmail.com>
2022-11-13 21:06:09 +0100

    country buttons
    country buttons - works!
    const clickedCountry - fixed
    add data.filter
    all should works fine now

--- src/index.js
+++ src/index.js
@@ -1,101 +1,103 @@
-    import './css/styles.css';
-    import { fetchCountries } from './fetchCountries';
-    import debounce from 'lodash.debounce';
-    import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
-    const inputCountry = document.querySelector('input#search-box');
-    const listCountry = document.querySelector('.country-list');
-    const infoCountry = document.querySelector('.country-info');
+const inputCountry = document.querySelector('input#search-box');
+const listCountry = document.querySelector('.country-list');
+const infoCountry = document.querySelector('.country-info');
 
-    const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 300;
 
-    //search countries database
-    function inputHandler(event) {
-        const searchInput = event.target.value.trim();
+//search countries database
+function inputHandler(event) {
+    const searchInput = event.target.value.trim();
                 
-        cleanCountry()
-        cleanListCountry()
+    cleanCountry()
+    cleanListCountry()
 
-        fetchCountries(searchInput)
-            .then(data => {
-                if (data.length > 10) {
-                    Notify.info('Too many matches found. Please enter a more specific name'
-                    );
-                    return;
-                } 
-                countryDataMarkup(data);
-            })
-            .catch(err => {
-                Notify.failure('Oops, there is no country with that name');
-            });
-    };
+    fetchCountries(searchInput)
+        .then(data => {
+            if (data.length > 10) {
+                Notify.info('Too many matches found. Please enter a more specific name'
+                );
+                return;
+            } 
+            countryDataMarkup(data);
+        })
+        .catch(err => {
+            Notify.failure('Oops, there is no country with that name');
+        });
+};
 
-    //creating countries list markup
-    function createListMarkup(data) {
+//creating countries list markup
+function createListMarkup(data) {
     
-        return data
-            .map(({ name, flags }) =>
-                `<li class="country-list__item" data-country='${name.common}'><img class="country-list__image" src="${flags.svg}" alt="${name.common}" height="40px" "/><p class="country-list__post">${name.common}</p></li>`
-            )
-            .join('');
+    return data
+        .map(({ name, flags }) =>
+            `<li class="country-list__item" data-country='${name.common}'><img class="country-list__image" src="${flags.svg}" alt="${name.common}" height="40px" "/><p class="country-list__post">${name.common}</p></li>`
+        )
+        .join('');
         
-    }; 
+}; 
 
-    //creating country info markup
-    function createDataMarkup(data) {
-        const countryEl = data[0];
-        const { name, capital, population, flags, languages } = countryEl;
-            return `
-                    <li class="country__item">
-                        <div class="country__flag-name-container">
-                            <img src="${flags.svg}" alt="${name.common}" height="30px"/></p>
-                            <h1 class="country__title">${name.official}</h2>
-                        </div>
-                        <p><b>Capital:</b> ${capital}
-                        <p><b>Population:</b> ${population}</p>
-                        <p><b>Languages:</b> ${Object.values(data[0].languages)}</p>
-                    </li>
-                    `;
-    };
+//creating country info markup
+function createDataMarkup(data) {
+    const countryEl = data[0];
+    const { name, capital, population, flags, languages } = countryEl;
+        return `
+                <li class="country__item">
+                    <div class="country__flag-name-container">
+                        <img src="${flags.svg}" alt="${name.common}" height="30px"/></p>
+                        <h1 class="country__title">${name.official}</h2>
+                    </div>
+                    <p><b>Capital:</b> ${capital}
+                    <p><b>Population:</b> ${population}</p>
+                    <p><b>Languages:</b> ${Object.values(data[0].languages)}</p>
+                </li>
+                `;
+};
 
-    //rendering
-    function countryDataMarkup(data) {
-        if (data.length === 1) {
-            const dataMarkup = createDataMarkup(data);
-            infoCountry.innerHTML = dataMarkup;
-        } else {
-            const listMarkup = createListMarkup(data);  
-            listCountry.innerHTML = listMarkup; 
+//rendering
+function countryDataMarkup(data) {
+    if (data.length === 1) {
+        const dataMarkup = createDataMarkup(data);
+        infoCountry.innerHTML = dataMarkup;
+    } else {
+        const listMarkup = createListMarkup(data);  
+        listCountry.innerHTML = listMarkup; 
 
-            //add click to country on list
-            const listCountryItem = document.querySelectorAll('li');
-           
-            listCountryItem.forEach(item => {
-                item.addEventListener('click', event => {
-                    const currentItem  = event.currentTarget;
-                    infoCountry.innerHTML = createDataMarkup(data);
-                    console.log('item clicked!', currentItem, event);
-                    cleanListCountry();
-                });
+        //add click to country on list
+        const listCountryItem = document.querySelectorAll('li');
+
+        listCountryItem.forEach(item => {
+            item.addEventListener('click', event => {
+                const clickedCountry = event.currentTarget.dataset.country;
+                //searches the database by clicked country
+                const wantedCountry = data.filter(
+                    country => country.name.common === clickedCountry
+                );
+
+                infoCountry.innerHTML = createDataMarkup(wantedCountry);
+                console.log('item clicked!', clickedCountry);
+                cleanListCountry();
             });
-        };
+        });
     };
+};
 
-        
+//input event listener 
+inputCountry.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
 
+//clear markup
+function cleanCountry() {
+    infoCountry.innerHTML = '';
+};
 
-    //input event listener 
-    inputCountry.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
-
-    //clear markup
-    function cleanCountry() {
-        infoCountry.innerHTML = '';
-    };
-
-    function cleanListCountry() {
-        listCountry.innerHTML = '';
-    }
+function cleanListCountry() {
+    listCountry.innerHTML = '';
+};
 
     
 
